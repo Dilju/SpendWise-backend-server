eb6175e23f878cdafc5e4bc5babcc57c59f2c67b
Dilju <diljubhaskaranam@gmail.com>
2025-09-11 11:35:19 +0530

    changes in forgotPassword in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -148,9 +148,10 @@ export const forgotPassword = async(req, res) => {
 
         // nodeMailer transport
         const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            secure: false,
+            // host: process.env.EMAIL_HOST,
+            // port: process.env.EMAIL_PORT,
+            service: "gmail",
+            // secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
